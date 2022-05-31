fc5ad925b01c22daab811e46dfcb54e87f15b00d
LudiJolo <joloh18@yahoo.com>
2022-05-30 21:23:35 -0700

    Homepage app
    - added new app "Homepage"
    - created additional paths
    - view function with parameter
    - fixed template settings

--- JOLO_WEBSITE/homepage/urls.py
+++ JOLO_WEBSITE/homepage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'homepage'
 #stores all urls that are associated with the app
 urlpatterns = [
-    path("", views.index, name="index")
+    path('', views.index, name='index'),
+    path("<str:name>", views.greet, name='greet')
 ]

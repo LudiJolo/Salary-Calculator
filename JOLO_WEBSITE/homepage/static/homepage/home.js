db4b13c87d277a1feb465f7a54526da5ebd27459
LudiJolo <joloh18@yahoo.com>
2022-06-29 20:51:36 -0700

    Fixed dark mode
    - website can toggle between light and dark mode

--- JOLO_WEBSITE/homepage/static/homepage/home.js
+++ JOLO_WEBSITE/homepage/static/homepage/home.js
@@ -3,27 +3,14 @@ $(document).ready(function(){
     function checkWidth(){
         var windowSize =  $(window).width();
         console.log(windowSize);
-        if (windowSize >= 768){
-            $('#header').text("John Paolo Hernandez");
-            $('#header2').text("Future Software Engineer");
+        if (windowSize > 991){
+            $('#navbar_container').attr('class','navbar-nav position-absolute top-50 start-50 translate-middle');
         }
         else{
-            $('#header').text("Jolo");
-            $('#header2').text("Software Engineer");
+            $('#navbar_container').attr('class','navbar-nav');
         }
     }
 
     checkWidth()
     $(window).resize(checkWidth);
 });
-
-var toggleDarkMode = function(ele){
-    if($('#flexSwitchCheckChecked').prop("checked") == true){
-        $('#global_body').attr('class', 'bg-dark text-light');
-        $('#global_nav').attr('class', 'navbar navbar-expand-lg bg-dark text-light');
-    }
-    else{
-        $('#global_body').attr('class', 'bg-light text-dark');
-        $('#global_nav').attr('class', 'navbar navbar-expand-lg bg-light text-dark');
-    }
-};
